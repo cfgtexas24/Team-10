a660976ecdfcaa0e9ad917fd9845b710265e17f2
miko quinones <miko@VJQ-D-4368K63.Virtual-Q.com>
2024-10-18 23:01:45 -0500

    Employee finished

--- CodeForGoodAPI/Program.cs
+++ CodeForGoodAPI/Program.cs
@@ -1,6 +1,7 @@
 using CodeForGoodAPI;
 using CodeForGoodAPI.Models;
 using CodeForGoodAPI.Services.Patients;
+using CodeForGoodAPI.Services.Employees;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,7 @@ string connectionString = builder.Configuration.GetConnectionString("DefaultConn
 
 #region Dependency Injection
 builder.Services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IPatientService, PatientService>();
 #endregion
 
