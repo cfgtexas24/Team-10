1898d5976676082e6a50f36ec274561bc6aa39fe
Shreya <shreyamekala01@gmail.com>
2024-10-19 05:33:24 -0500

    Updated Service Table and Functionality

--- patient-portal/src/app/dashboard/page.js
+++ patient-portal/src/app/dashboard/page.js
@@ -3,63 +3,46 @@ import { useState } from "react";
 import { FiMenu, FiArrowLeft } from "react-icons/fi"; // Importing icons from react-icons
 
 const patientData = [
-  {
-    id: 1,
-    firstName: "Emily",
-    lastName: "Johnson",
-    emailAddress: "emily.johnson@example.com",
-    dateOfBirth: "1990-05-14",
-    age: 34,
-    occupation: "Teacher",
-    race: "White",
-    ethnicity: "Non-Hispanic",
-    insurance: "Yes",
-  },
-  {
-    id: 2,
-    firstName: "Sophia",
-    lastName: "Brown",
-    emailAddress: "sophia.brown@example.com",
-    dateOfBirth: "1985-11-23",
-    age: 38,
-    occupation: "Nurse",
-    race: "Black or African American",
-    ethnicity: "Non-Hispanic",
-    insurance: "No",
-  },
-  // ... other patient records
+  { id: 1, firstName: 'Emily', lastName: 'Johnson', emailAddress: 'emily.johnson@example.com', dateOfBirth: '1990-05-14', age: 34, occupation: 'Teacher', race: 'White', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 2, firstName: 'Sophia', lastName: 'Brown', emailAddress: 'sophia.brown@example.com', dateOfBirth: '1985-11-23', age: 38, occupation: 'Nurse', race: 'Black or African American', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 3, firstName: 'Isabella', lastName: 'Martinez', emailAddress: 'isabella.martinez@example.com', dateOfBirth: '1992-03-02', age: 32, occupation: 'Software Engineer', race: 'Asian', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 4, firstName: 'Mia', lastName: 'Garcia', emailAddress: 'mia.garcia@example.com', dateOfBirth: '1996-07-19', age: 28, occupation: 'Accountant', race: 'White', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 5, firstName: 'Amelia', lastName: 'Davis', emailAddress: 'amelia.davis@example.com', dateOfBirth: '1991-12-10', age: 33, occupation: 'Marketing Specialist', race: 'Black or African American', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 6, firstName: 'Charlotte', lastName: 'Miller', emailAddress: 'charlotte.miller@example.com', dateOfBirth: '1987-04-25', age: 37, occupation: 'Architect', race: 'Asian', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 7, firstName: 'Harper', lastName: 'Rodriguez', emailAddress: 'harper.rodriguez@example.com', dateOfBirth: '1995-06-17', age: 29, occupation: 'Pharmacist', race: 'White', ethnicity: 'Hispanic', insurance: 'No' },
+  { id: 8, firstName: 'Evelyn', lastName: 'Martinez', emailAddress: 'evelyn.martinez@example.com', dateOfBirth: '1988-09-03', age: 36, occupation: 'Graphic Designer', race: 'Native Hawaiian or Other Pacific Islander', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 9, firstName: 'Abigail', lastName: 'Lee', emailAddress: 'abigail.lee@example.com', dateOfBirth: '1993-01-15', age: 31, occupation: 'Social Worker', race: 'Asian', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 10, firstName: 'Avery', lastName: 'Perez', emailAddress: 'avery.perez@example.com', dateOfBirth: '1997-08-12', age: 27, occupation: 'Lawyer', race: 'White', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 11, firstName: 'Sofia', lastName: 'Lopez', emailAddress: 'sofia.lopez@example.com', dateOfBirth: '1989-02-09', age: 35, occupation: 'Data Scientist', race: 'Hispanic', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 12, firstName: 'Liam', lastName: 'Martinez', emailAddress: 'liam.martinez@example.com', dateOfBirth: '1994-07-22', age: 30, occupation: 'Civil Engineer', race: 'White', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 13, firstName: 'Lucas', lastName: 'Hernandez', emailAddress: 'lucas.hernandez@example.com', dateOfBirth: '1986-03-14', age: 38, occupation: 'Physician', race: 'Asian', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 14, firstName: 'Mason', lastName: 'Anderson', emailAddress: 'mason.anderson@example.com', dateOfBirth: '1990-12-05', age: 33, occupation: 'Web Developer', race: 'Black or African American', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 15, firstName: 'Ava', lastName: 'Thomas', emailAddress: 'ava.thomas@example.com', dateOfBirth: '1995-11-19', age: 28, occupation: 'Biologist', race: 'White', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 16, firstName: 'Ella', lastName: 'Wilson', emailAddress: 'ella.wilson@example.com', dateOfBirth: '1992-08-23', age: 31, occupation: 'Veterinarian', race: 'Asian', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 17, firstName: 'Scarlett', lastName: 'Moore', emailAddress: 'scarlett.moore@example.com', dateOfBirth: '1988-10-14', age: 35, occupation: 'Physical Therapist', race: 'Black or African American', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 18, firstName: 'Grace', lastName: 'Taylor', emailAddress: 'grace.taylor@example.com', dateOfBirth: '1996-04-20', age: 28, occupation: 'Chef', race: 'Native Hawaiian or Other Pacific Islander', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 19, firstName: 'Chloe', lastName: 'Anderson', emailAddress: 'chloe.anderson@example.com', dateOfBirth: '1993-09-30', age: 30, occupation: 'Graphic Artist', race: 'White', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 20, firstName: 'Ethan', lastName: 'Davis', emailAddress: 'ethan.davis@example.com', dateOfBirth: '1984-05-11', age: 40, occupation: 'Financial Analyst', race: 'Black or African American', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 21, firstName: 'Henry', lastName: 'Gonzalez', emailAddress: 'henry.gonzalez@example.com', dateOfBirth: '1991-07-03', age: 33, occupation: 'Operations Manager', race: 'Asian', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 22, firstName: 'Oliver', lastName: 'Parker', emailAddress: 'oliver.parker@example.com', dateOfBirth: '1995-01-27', age: 29, occupation: 'Project Manager', race: 'White', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 23, firstName: 'Jack', lastName: 'Johnson', emailAddress: 'jack.johnson@example.com', dateOfBirth: '1992-02-18', age: 32, occupation: 'Network Engineer', race: 'Black or African American', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 24, firstName: 'Luna', lastName: 'Garcia', emailAddress: 'luna.garcia@example.com', dateOfBirth: '1994-08-21', age: 29, occupation: 'UX Designer', race: 'Native Hawaiian or Other Pacific Islander', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 25, firstName: 'Aiden', lastName: 'Lopez', emailAddress: 'aiden.lopez@example.com', dateOfBirth: '1989-06-12', age: 35, occupation: 'Insurance Agent', race: 'White', ethnicity: 'Hispanic', insurance: 'No' },
+  { id: 26, firstName: 'James', lastName: 'Martinez', emailAddress: 'james.martinez@example.com', dateOfBirth: '1983-10-30', age: 40, occupation: 'Teacher', race: 'Asian', ethnicity: 'Non-Hispanic', insurance: 'Yes' },
+  { id: 27, firstName: 'Mila', lastName: 'Brown', emailAddress: 'mila.brown@example.com', dateOfBirth: '1992-01-16', age: 32, occupation: 'Research Scientist', race: 'Black or African American', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 28, firstName: 'Ella', lastName: 'Wilson', emailAddress: 'ella.wilson@example.com', dateOfBirth: '1996-05-15', age: 28, occupation: 'Quality Assurance Tester', race: 'White', ethnicity: 'Non-Hispanic', insurance: 'No' },
+  { id: 29, firstName: 'Zoe', lastName: 'Thompson', emailAddress: 'zoe.thompson@example.com', dateOfBirth: '1984-04-21', age: 40, occupation: 'Software Engineer', race: 'Asian', ethnicity: 'Hispanic', insurance: 'Yes' },
+  { id: 30, firstName: 'Liam', lastName: 'Williams', emailAddress: 'liam.williams@example.com', dateOfBirth: '1989-02-14', age: 35, occupation: 'Business Analyst', race: 'Black or African American', ethnicity: 'Non-Hispanic', insurance: 'No' },
 ];
 
 const serviceData = [
-  {
-    id: 1,
-    dateAttended: "2024-01-15",
-    serviceType: "Pregnancy Test",
-    patientFirstName: "Emily",
-    patientLastName: "Johnson",
-    dateOfBirth: "1990-05-14",
-    report: "report1.pdf",
-  },
-  {
-    id: 2,
-    dateAttended: "2024-01-20",
-    serviceType: "Ultrasound",
-    patientFirstName: "Sophia",
-    patientLastName: "Brown",
-    dateOfBirth: "1985-11-23",
-    report: "report2.pdf",
-  },
-  {
-    id: 3,
-    dateAttended: "2024-01-25",
-    serviceType: "Prenatal Care",
-    patientFirstName: "Isabella",
-    patientLastName: "Martinez",
-    dateOfBirth: "1992-03-02",
-    report: "report3.pdf",
-  },
-  // ... other service records
+  { id: 1, dateAttended: '2024-01-15', serviceType: 'Pregnancy Test', patientFirstName: 'Emily', patientLastName: 'Johnson', dateOfBirth: '1990-05-14', report: 'report1.pdf' },
+  { id: 2, dateAttended: '2024-01-20', serviceType: 'Ultrasound', patientFirstName: 'Sophia', patientLastName: 'Brown', dateOfBirth: '1985-11-23', report: 'report2.pdf' },
+  { id: 3, dateAttended: '2024-01-25', serviceType: 'Prenatal Care', patientFirstName: 'Isabella', patientLastName: 'Martinez', dateOfBirth: '1992-03-02', report: 'report3.pdf' },
+  { id: 4, dateAttended: '2024-02-01', serviceType: 'Postpartum Care', patientFirstName: 'Mia', patientLastName: 'Garcia', dateOfBirth: '1996-07-19', report: 'report4.pdf' },
+  { id: 5, dateAttended: '2024-02-05', serviceType: 'Group Prenatal Care', patientFirstName: 'Amelia', patientLastName: 'Davis', dateOfBirth: '1991-12-10', report: 'report5.pdf' },
 ];
+
 
 const AdminDashboard = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -81,7 +64,7 @@ const AdminDashboard = () => {
   });
 
   // State for sidebar visibility and selected tab
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [selectedTab, setSelectedTab] = useState("By Patient");
 
   const handleSearch = (e) => {
@@ -152,6 +135,29 @@ const AdminDashboard = () => {
     ? ((filteredServiceRecordsCount / totalServiceRecords) * 100).toFixed(2)
     : 0;
 
+
+    const [newService, setNewService] = useState({
+      serviceType: '',
+      patientFirstName: '',
+      patientLastName: '',
+      dateOfBirth: '',
+      dateAttended: '',
+      report: ''
+    });
+    
+    const handleNewServiceChange = (e) => {
+      const { name, value } = e.target;
+      setNewService((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    };
+    
+    const handleAddService = () => {
+      console.log(newService);
+    };
+
+    
   return (
     <div className="flex h-screen">
       {/* Collapsible Sidebar */}
@@ -161,7 +167,7 @@ const AdminDashboard = () => {
         } transition-width duration-300 bg-gray-100 h-full overflow-hidden`}
       >
         {isSidebarOpen && (
-          <div className="p-6">
+          <div className="p-6 space-y-6">
             <h2 className="text-xl font-semibold mb-4">Select Tab</h2>
             <button
               onClick={() => setSelectedTab("By Patient")}
@@ -169,16 +175,8 @@ const AdminDashboard = () => {
             >
               By Patient
             </button>
-            <button
-              onClick={() => setSelectedTab("By Service")}
-              className="block"
-            >
-              By Service
-            </button>
-
             {selectedTab === "By Patient" && (
               <>
-                <h2 className="text-xl font-semibold mb-4">Filter Patients</h2>
                 <div className="space-y-4">
                   <select
                     name="race"
@@ -224,6 +222,17 @@ const AdminDashboard = () => {
                 </div>
               </>
             )}
+            <button
+              onClick={() => setSelectedTab("By Service")}
+              className="block"
+            >
+              <div className="space-y-4">
+              By Service
+              </div>
+              
+            </button>
+
+           
           </div>
         )}
       </div>
@@ -292,6 +301,84 @@ const AdminDashboard = () => {
                 patient records: {patientPercentage}% of patients
               </p>
             </div>
+              {/* Add New Patient */}
+              <div>
+              <h2 className="text-xl font-semibold mb-4">Add New Patient</h2>
+              <div className="grid grid-cols-2 gap-4">
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={newUser.firstName}
+                  onChange={handleNewUserChange}
+                  className="input"
+                />
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={newUser.lastName}
+                  onChange={handleNewUserChange}
+                  className="input"
+                />
+                <input
+                  type="email"
+                  name="emailAddress"
+                  placeholder="Email"
+                  value={newUser.emailAddress}
+                  onChange={handleNewUserChange}
+                  className="input"
+                />
+                <input
+                  type="date"
+                  name="dateOfBirth"
+                  placeholder="Date of Birth"
+                  value={newUser.dateOfBirth}
+                  onChange={handleNewUserChange}
+                  className="input"
+                />
+                <input
+                  type="text"
+                  name="race"
+                  placeholder="Race"
+                  value={newUser.race}
+                  onChange={handleNewUserChange}
+                  className="input"
+                />
+                <input
+                  type="text"
+                  name="ethnicity"
+                  placeholder="Ethnicity"
+                  value={newUser.ethnicity}
+                  onChange={handleNewUserChange}
+                  className="input"
+                />
+                <input
+                  type="text"
+                  name="occupation"
+                  placeholder="Occupation"
+                  value={newUser.occupation}
+                  onChange={handleNewUserChange}
+                  className="input"
+                />
+                <select
+                  name="insurance"
+                  value={newUser.insurance}
+                  onChange={handleNewUserChange}
+                  className="input"
+                >
+                  <option value="">Select Insurance</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+              <button
+                onClick={handleAddUser}
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+              >
+                Add Patient
+              </button>
+            </div>
           </>
         ) : (
           <>
@@ -347,6 +434,66 @@ const AdminDashboard = () => {
                 service records: {servicePercentage}% of services
               </p>
             </div>
+            {/* Add New Service */}
+            <div>
+              <h2 className="text-xl font-semibold mb-4">Add New Service</h2>
+              <div className="grid grid-cols-2 gap-4">
+                <input
+                  type="text"
+                  name="serviceType"
+                  placeholder="Service Type"
+                  value={newService.serviceType}
+                  onChange={handleNewServiceChange}
+                  className="input"
+                />
+                <input
+                  type="text"
+                  name="patientFirstName"
+                  placeholder="Patient First Name"
+                  value={newService.patientFirstName}
+                  onChange={handleNewServiceChange}
+                  className="input"
+                />
+                <input
+                  type="text"
+                  name="patientLastName"
+                  placeholder="Patient Last Name"
+                  value={newService.patientLastName}
+                  onChange={handleNewServiceChange}
+                  className="input"
+                />
+                <input
+                  type="date"
+                  name="dateOfBirth"
+                  placeholder="Date of Birth"
+                  value={newService.dateOfBirth}
+                  onChange={handleNewServiceChange}
+                  className="input"
+                />
+                <input
+                  type="date"
+                  name="dateAttended"
+                  placeholder="Date Attended"
+                  value={newService.dateAttended}
+                  onChange={handleNewServiceChange}
+                  className="input"
+                />
+                <input
+                  type="file"
+                  name="report"
+                  placeholder="Report"
+                  onChange={handleNewServiceChange}
+                  className="input"
+                />
+              </div>
+              <button
+                onClick={handleAddService}
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+              >
+                Add Service
+              </button>
+            </div>
+
           </>
         )}
       </div>
